58f9da35bb38c38b32dc9755ada2966f629b7770
Codysseus23 <Codysseus23@github.com>
2020-08-27 10:08:26 -0400

    Added rpg

--- rpg-0.py
+++ rpg-0.py
@@ -7,15 +7,60 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 
 """
 
-def main():
-    hero_health = 10
-    hero_power = 5
-    goblin_health = 6
-    goblin_power = 2
+class Hero:
+    
+    
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+        
 
-    while goblin_health > 0 and hero_health > 0:
-        print("You have %d health and %d power." % (hero_health, hero_power))
-        print("The goblin has %d health and %d power." % (goblin_health, goblin_power))
+    def attack(self, target):
+        target.health -= self.power
+        print("You do %d damage to the goblin." % self.power)
+
+    def alive(self):
+        self.health > 0
+        return self.health
+
+    def status(self):
+        print("You have %d health and %d power." % (self.health, self.power))
+
+
+
+
+class Goblin:
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+    
+    def attack(self, target):
+        target.health -= self.power
+        print("You do %d damage to the hero." % self.power)
+    
+    def alive(self):
+        self.health > 0
+        return self.health
+
+    def status(self):
+        print("The goblin has %d health and %d power." % (self.health, self.power))
+
+
+
+Codysseus = Hero(10, 5)
+Krenko = Goblin(6, 2)
+
+
+
+def main():
+    
+
+    
+    
+    
+    while Krenko.alive() > 0 and Codysseus.alive() > 0:
+        Codysseus.status()
+        Krenko.status()
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -25,9 +70,9 @@ def main():
         user_input = input()
         if user_input == "1":
             # Hero attacks goblin
-            goblin_health -= hero_power
-            print("You do %d damage to the goblin." % hero_power)
-            if goblin_health <= 0:
+            Codysseus.attack(Krenko)
+            
+            if Krenko.health <= 0:
                 print("The goblin is dead.")
         elif user_input == "2":
             pass
@@ -37,11 +82,11 @@ def main():
         else:
             print("Invalid input %r" % user_input)
 
-        if goblin_health > 0:
+        if Krenko.health > 0:
             # Goblin attacks hero
-            hero_health -= goblin_power
-            print("The goblin does %d damage to you." % goblin_power)
-            if hero_health <= 0:
+            Krenko.attack(Codysseus)
+            
+            if Codysseus.health <= 0:
                 print("You are dead.")
 
 main()
